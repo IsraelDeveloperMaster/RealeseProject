563536715bf81d3972a066e11e646837397bb874
Israel Santos <dev@developermaster.net>
2024-12-04 12:10:27 +0100

    feat: Add initial project files
    Added initial project files for the Android project, including:
    - MainActivity setup with basic composable functions
    - Theme setup
    - Edge-to-edge display configuration

---
+++ app/src/main/java/net/developermaster/realeseproject/MainActivity.kt
@@ -0,0 +1,51 @@
+package net.developermaster.realeseproject
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import net.developermaster.realeseproject.ui.theme.RealeseProjectTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            RealeseProjectTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello! $name!",
+        style = MaterialTheme.typography.headlineLarge,
+
+
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    RealeseProjectTheme {
+        Greeting("Android")
+    }
+}
